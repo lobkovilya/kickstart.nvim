0f3c469b4f60ef5e6d0e150da61283ed2374c273
Ilya Lobkov <ilya.lobkov@konghq.com>
2025-11-11 12:55:11 +0100

    <leader>bd to delete the buffer

--- init.lua
+++ init.lua
@@ -106,6 +106,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Delete current buffer
+vim.keymap.set('n', '<leader>bd', '<cmd>bdelete<CR>', { desc = '[B]uffer [D]elete' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 

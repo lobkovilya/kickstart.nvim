e649f5314d2d4886bebcc658fc7cb560c708b774
Ilya Lobkov <ilya.lobkov@konghq.com>
2026-03-13 10:05:52 +0100

    remove opencode

--- init.lua
+++ init.lua
@@ -1179,26 +1179,6 @@ require('lazy').setup({
     end,
   },
 
-  {
-    'nickjvandyke/opencode.nvim',
-    version = '*',
-    config = function()
-      vim.g.opencode_opts = {}
-      vim.o.autoread = true
-
-      vim.keymap.set({ 'n', 'x' }, '<leader>oa', function()
-        require('opencode').ask('@this: ', { submit = true })
-      end, { desc = '[O]pencode [A]sk' })
-
-      vim.keymap.set({ 'n', 'x' }, '<leader>os', function()
-        require('opencode').select()
-      end, { desc = '[O]pencode [S]elect action' })
-
-      vim.keymap.set({ 'n', 'x' }, '<leader>or', function()
-        return require('opencode').operator '@this '
-      end, { desc = '[O]pencode add [R]ange', expr = true })
-    end,
-  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
